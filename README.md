b729ea7c7125287b261d6ae476f92968921ffad1
Syed Uzair Rehan <iamsyeduzairshah@gmail.com>
2025-05-04 13:32:00 +0500

    update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # 36-weeks-preparation
  36 weeks remote job preparation with Sir Saif.
+
+## Course content link
+ (https://quaint-dracorex-77b.notion.site/SYLLABUS-36-WEEKS-PREP-By-Saif-1bb008f4cf9380a1977ddb8292d70d47)[content]
--- assignments/01/easy/anagram.js
+++ assignments/01/easy/anagram.js
@@ -5,7 +5,14 @@
 */
 
 function isAnagram(str1, str2) {
-
+  let new1 = str1.split("").sort().join("");
+  let new2 = str2.split("").sort().join("");
+  if (new1 == new2) {
+    console.log(true);
+    return true;
+  }
+  console.log(false);
+  return false;
 }
 
 module.exports = isAnagram;
